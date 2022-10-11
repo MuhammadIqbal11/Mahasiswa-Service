d229804c230228219a1f196f211a2e845eab7440
Muhammad Iqbal <113502513+MuhammadIqbal11@users.noreply.github.com>
2022-10-12 05:35:53 +0700

    Add files via upload

---
+++ src/test/java/com/iqbal/user/mahasiswa/UserMahasiswaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.iqbal.user.mahasiswa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserMahasiswaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
